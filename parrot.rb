831b00f24363b38fb7564de8bd268d9c65e0dbd7
Cathy Burbridge <cathyburbridge@outlook.com>
2018-12-18 13:16:36 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,9 @@
-Parrot = "Squawk!"
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(sound="Squawk!")
+  puts sound
+  return sound
+end
+
+parrot("Pretty bird!")
+parrot 
